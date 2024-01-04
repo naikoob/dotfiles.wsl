d4ad891d51fb6f42e827cd619e387f613add9749
bookian <bookian@gmail.com>
2024-01-04 17:38:34 +0800

    fixed wsl(ubuntu) gcloud directory

--- zsh/.zsh/modules/gcloud.zsh
+++ zsh/.zsh/modules/gcloud.zsh
@@ -1,10 +1,9 @@
 # google cloud sdk
-GCLOUD_SDK_HOME=/opt/google-cloud-sdk
+GCLOUD_SHARE_HOME=/usr/share/google-cloud-sdk
 
 include() {
     [[ -f "$1" ]] && source "$1"
 }
 
-include $GCLOUD_SDK_HOME/path.zsh.inc
-include $GCLOUD_SDK_HOME/completion.zsh.inc
+include $GCLOUD_SHARE_HOME/completion.zsh.inc
 
